90a356af069157f00d0972c143ee901379f5855b
Luisru9 <lrgp42@outlook.com>
2024-01-23 14:11:48 -0400

    hexadecimals

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
+number = 0
+for number in range(0, 99):
+    print("{:d} = 0x{:x}".format(number, number))
+    number += 1
