274bc5962730d8b1b936eee2ae15a157fb90f8b7
Luisru9 <lrgp42@outlook.com>
2024-01-26 12:11:00 -0400

    infinite adding

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,10 +4,10 @@ import sys
 
 def main():
     total = 0
-    for arg in sys.argv{1:}:
-    total += int(arg)
+    for arg in sys.argv[1:]:
+        total += int(arg)
     print(total)
 
 
-    if __name__ == "__main__"
+if __name__ == "__main__":
         main()
