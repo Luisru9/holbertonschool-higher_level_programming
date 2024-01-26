022362ad454c290968a2db4dc7ddc7f9e42cba31
Luisru9 <lrgp42@outlook.com>
2024-01-26 10:12:02 -0400

    calculation

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calc
+    # * asing values to a and b
+a = 10
+b = 5
+print(f"{a} + {b} = {calc.add(a, b)}")
+print(f"{a} + {b} = {calc.sub(a, b)}")
+print(f"{a} + {b} = {calc.mul(a, b)}")
+print(f"{a} + {b} = {calc.div(a, b)}")
