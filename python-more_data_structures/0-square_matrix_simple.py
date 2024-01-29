1f69dfa0faf5111e0f2b3d166380156f191ae681
Luisru9 <lrgp42@outlook.com>
2024-01-29 19:35:00 -0400

    fixing code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-def square_matric_simple(matrix=[]):
-    # Create a new matrix with same size as input matrix
-    result_matrix = []
-
-    for row in matrix:
-        # For each row in the input matrix
-        new_row = [x ** 2 for x in row]
-        # Square each value in the row and append to result matrix
-        result_matrix.append(new_row)
-
-        return result_matrix
+def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        return None
+    m_copy = matrix.copy()
+    return list(map(lambda row:
+                    list(map(lambda colm: colm ** 2, row)), m_copy))
