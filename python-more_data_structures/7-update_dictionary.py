b7eb9bb5bef67f2d0922b6565a261f912aa3492f
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:56:56 -0400

    updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    new_dict[key] = value
-    return new_dict
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+    else:
+        a_dictionary.setdefault(key, value)
+        return a_dictionary
