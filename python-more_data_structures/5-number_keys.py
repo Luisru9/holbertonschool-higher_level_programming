afccd7e4d03d1bda44d067cb76c9939c6de7d723
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:27:35 -0400

    len of dictionary

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-def number_key(a_dictionary):
-    # Use the len() function to get the number of keys
-    return number_key
+def number_keys(a_dictionary):
+    # Initialize a counter variable
+    num_keys = 0
+    # Iterate through the keys of the dictionary
+    for key in a_dictionary:
+        # Increment the counter for each key
+        num_keys += 1
+
+    return num_keys
