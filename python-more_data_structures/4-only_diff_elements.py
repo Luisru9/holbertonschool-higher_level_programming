e63c7e52a00360d472a73de58f6226665b652b39
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:18:36 -0400

    only diff

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def onl(set_1, set_2):
-    return set_1.symmetric_difference(set_2)
+def only_diff_elements(set_1, set_2):
+    # Find elements present in only one set using set operations
+    only_diff_set = (set_1 - set_2) | (set_2 - set_1)
+    return only_diff_set
