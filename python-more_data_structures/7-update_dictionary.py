30d86f292f0f0c92b34e929a3b119ca420419288
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:49:20 -0400

    updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    # Use the setdefault method to replace or add key-value pairs
-    a_dictionary.setdefault(key, value)
-    return a_dictionary
+    # Update the value if the key already exists, otherwise add a new key-value pair
+    a_dictionary[key] = value
