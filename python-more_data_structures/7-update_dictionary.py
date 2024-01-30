a5843ae55dc34b65475c07bb1bc6524d4fed9919
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:46:04 -0400

    updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,3 @@
 def update_dictionary(a_dictionary, key, value):
     # Use dictionary unpacking to replace or add key-value pairs
     a_dictionary = {**a_dictionary, key: value}
-
-
-# Example usage:
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-update_dictionary(my_dict, 'b', 4)  # Update existing key 'b'
-update_dictionary(my_dict, 'd', 5)  # Add new key 'd'
-update_dictionary(my_dict, 'a', 10)  # Update existing key 'a'
