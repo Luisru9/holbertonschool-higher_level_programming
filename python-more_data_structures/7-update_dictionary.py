ad755b537231403db3734cf4602676d3f99aa2e2
Luisru9 <lrgp42@outlook.com>
2024-01-30 19:47:51 -0400

    updating dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    # Use dictionary unpacking to replace or add key-value pairs
-    a_dictionary = {**a_dictionary, key: value}
+    # Use the setdefault method to replace or add key-value pairs
+    a_dictionary.setdefault(key, value)
+    return a_dictionary
