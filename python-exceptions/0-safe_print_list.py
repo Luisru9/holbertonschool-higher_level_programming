e2cb9f63e2215beb2a8489fea8d762b1496f1eb2
Luisru9 <lrgp42@outlook.com>
2024-01-30 20:46:06 -0400

    element x print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):
-            print(my_list[i], end="")
+        for element in my_list[:x]:
+            print(element, end="")
             count += 1
     except IndexError:
         pass
