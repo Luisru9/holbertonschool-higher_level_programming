48da9ff942e82608d2dbbb7a3732d24232a44d29
Luisru9 <lrgp42@outlook.com>
2024-01-30 22:15:52 -0400

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,15 @@ def safe_print_list(my_list=[], x=0):
     if not my_list or x == 0:
         print()
         return 0
+    i = 0
     try:
         for i in range(0, x):
-            print(f"{my_list[i]}".format(my_list[i]), end="")
-        print()
-        return i + 1
-    except (IndexError, TypeError):
+            print("{}".format(my_list[i]), end="")
+            # Check for IndexError and break if needed
+            if i >= len(my_list) - 1:
+                break
+    except TypeError:
+        pass
+    finally:
         print("")
-        return i
+    return i + 1
