5abd51fd1e272eefc49b7f215d55c9f2951720dc
Luisru9 <lrgp42@outlook.com>
2024-01-30 22:10:33 -0400

    print division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Error: Division by zero")
-        result = None
-    except Exception as e:
-        print(f"Error: {e}")
+    except (ZeroDivisionError, TypeError):
         result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
