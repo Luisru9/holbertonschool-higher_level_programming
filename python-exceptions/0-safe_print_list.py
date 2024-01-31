0078391a689009ee214b82ebe2a07fe7958af46f
Luisru9 <lrgp42@outlook.com>
2024-01-30 21:16:15 -0400

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 def safe_print_list(my_list=[], x=0):
     try:
-        for element in my_list[:x]:
-            print(element, end="")
+        count = 0
+        for i in range(x):
+            print(my_list[i], end=" ")
+            count += 1
     except IndexError:
         pass
     finally:
         print()
-    return min(x, len(my_list))
+        return count
