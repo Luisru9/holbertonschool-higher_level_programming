f698931b0fc6d42b969c97dff7f54d763ce0ee41
Luisru9 <lrgp42@outlook.com>
2024-01-30 22:03:13 -0400

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 def safe_print_list(my_list=[], x=0):
-    count_elements = 0
-    try:
-        for i in range(x):
-            # Replace the print statement with an empty string
-            result_str = "{} ".format(my_list[i])
-            count_elements += 1
-    except IndexError:
-        pass
-    finally:
-        # Print the result string outside the loop
-        print(result_str)
-    return count_elements
+    num = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            num += 1
+        except IndexError:
+            break
+    print("")  # Moved outside the loop
+    return num
