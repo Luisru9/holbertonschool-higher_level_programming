a5167c30b4b47f08a28e4fd5d4e7b5e8a7a294b7
Luisru9 <lrgp42@outlook.com>
2024-01-31 20:54:51 -0400

    define square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-# This is a module containing a class representing a square
+"""This is a module containing a class for representing a Square."""
+
+
 class Square:
-    # A class representing a square
-    def __init__(self):
-        pass
+    """A class representing a square.
+
+    This class is currently empty and can be used as a placeholder.
+    It can be extended to include attributes and methods for representing
+    and manipulating square objects.
+    """
+    pass
