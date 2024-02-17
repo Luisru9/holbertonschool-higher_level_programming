ccf3bc593208ac197abdc465fb8e133e968c6d27
Luisru9 <lrgp42@outlook.com>
2024-02-17 16:31:59 -0400

    fixing test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-"""Defines test classes for Base class"""
-
+"""Unittests for Base class"""
 import unittest
-from io import StringIO
-import os
-from unittest.mock import patch
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def test_assigning_automatically_id(self):
-        self.assertEqual(Base().id, 1)
+    """Test cases for Base class"""
 
-    # Other test cases for Base class
+    def test_id(self):
+        """Test id assignment"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+        b5 = Base(0)
+        self.assertEqual(b5.id, 0)
+
+    def test_no_args(self):
+        """Test with no arguments"""
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_none_id(self):
+        """Test with None as id"""
+        b = Base(None)
+        self.assertEqual(b.id, 1)
+
+    def test_str(self):
+        """Test __str__ method"""
+        b = Base(42)
+        self.assertEqual(str(b), "[Base] (42)")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
