b9d31a01e4ca77a2be0860c2e28cdb9ee2b5fd77
Luisru9 <lrgp42@outlook.com>
2024-03-15 20:56:02 -0400

    ID not null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- Unique ID
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- Id not null
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
