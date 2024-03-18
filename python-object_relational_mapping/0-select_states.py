4796eecc533b778a0e51912981a6edd99e49343c
Luisru9 <lrgp42@outlook.com>
2024-03-18 08:52:30 -0400

    list all states from other database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script list all states from a database"""
+
+if_name_ == '_main_':
+import MySQLdb
+import sys
+
+username, password, database_name = sys.argv[1:]
+
+db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name)
+cur = db.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY id")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
